54badccc45cd030fec097f6f42b52e0bb9021243
Addis Negash <anegash154@gmail.com>
2025-06-01 09:52:14 +0300

    feat:select the modal elements and each images by dom selector

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded',()=>{
-     
+     const modal=document.getElementById('modal');
+    const modaltext=document.getElementById('name-of-image');
+    const modaldate=document.getElementById('date-of-image');
+    const motivation=document.getElementById('motivation');
+    const modalimage=document.getElementById('modal-image');
 
+    let photos=document.querySelectorAll('photo-item');
 });

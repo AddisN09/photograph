5851a2f49a0ca2a408f2981713f8a483cee618a2
Addis Negash <anegash154@gmail.com>
2025-06-03 18:45:37 +0300

    fix

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
-document.addEventListener('DOMContentLoaded',()=>{
-     const modal=document.getElementById('modal');
-    const modaltext=document.getElementById('name-of-image');
-    const modaldate=document.getElementById('date-of-image');
-    const motivation=document.getElementById('motivation');
-    const modalimage=document.getElementById('modal-image');
-  if(modal){
-     let photos=document.querySelectorAll('.photo-item');
-    photos.forEach(photo=>{
-        photo.addEventListener('click',()=>{
-            modaltext.textContent=photo.dataset.names;
-            modaldate.textContent=photo.dataset.date;
-            motivation.textContent=photo.dataset.motivotion;
-            modalimage.src=photo.dataset.image;
-            modal.style.display='grid';
+document.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById('modal');
+    const modaltext = document.getElementById('name-of-image');
+    const modaldate = document.getElementById('date-of-image');
+    const motivation = document.getElementById('motivation');
+    const modalimage = document.getElementById('modal-image');
+
+    
+        let photos = document.querySelectorAll('.photo-item');
+        photos.forEach(photo => {
+            photo.addEventListener('click', () => {
+                modaltext.textContent = photo.dataset.names;
+                modaldate.textContent = photo.dataset.date;
+                motivation.textContent = photo.dataset.motivotion;
+                modalimage.src = photo.dataset.image;
+                modal.style.display = 'grid';
+            });
         });
-    });
-    modal.addEventListener('click',e=>{
-        if(e.target===modal){
-            modal.style.display='none';
-        }
-    });
-
-  }
-
-const form=document.getElementsByClassName('form-container')[0];
-const text=document.getElementById('popuptext');
-
-
-if(form){
-form.addEventListener('submit',e=>{
-    e.preventDefault();
-    text.textContent="Thank you for your message! I'll get back to you soon.";
-    form.reset();
-});
-}
-
-
+        modal.addEventListener('click', e => {
+            if (e.target === modal) {
+                modal.style.display = 'none';
+            }
+        });
 });
